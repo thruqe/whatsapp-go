7c5bb1d88a27c34ae263331b161ef16e20e1866b
Thruqe <danielpeter0039@gmail.com>
2026-08-25 21:47:19 +0100

    refactor: cli base

--- cli/bot.go
+++ cli/bot.go
@@ -676,3 +676,34 @@ func (b *Bot) WAEventHandler(evt any) {
 		Logger.Debug("unhandled event received", "type", fmt.Sprintf("%T", evt), "event", evt)
 	}
 }
+
+func buildIncomingMessagePayload(v *events.Message) IncomingMessagePayload {
+	text := utils.ExtractMessageText(v)
+	mediaType := utils.GetMediaType(v.Message)
+
+	var quotedID string
+	var quotedText string
+
+	if ext := v.Message.GetExtendedTextMessage(); ext != nil && ext.GetContextInfo() != nil {
+		ci := ext.GetContextInfo()
+		quotedID = ci.GetStanzaID()
+		if ci.QuotedMessage != nil {
+			quotedText = utils.ExtractTextFromProto(ci.QuotedMessage)
+		}
+	}
+
+	return IncomingMessagePayload{
+		From:       v.Info.Chat.String(),
+		Chat:       v.Info.Chat.String(),
+		Sender:     v.Info.Sender.String(),
+		Text:       text,
+		MessageID:  v.Info.ID,
+		PushName:   v.Info.PushName,
+		Timestamp:  v.Info.Timestamp,
+		IsGroup:    v.Info.IsGroup,
+		IsFromMe:   v.Info.IsFromMe,
+		MediaType:  mediaType,
+		QuotedID:   quotedID,
+		QuotedText: quotedText,
+	}
+}
